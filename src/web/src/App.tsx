43627454f9a84d5a83e49c881a5af0505ba14c93
silentoaq <silentoaq@gmail.com>
2025-07-19 20:17:25 +0800

    chore : psuh

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,10 +1,11 @@
+import { ThemeProvider } from "@/components/theme-provider"
 import { Button } from "@/components/ui/button"
-
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"><div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
-    </div>
+    </div></ThemeProvider>
+
   )
 }
 
