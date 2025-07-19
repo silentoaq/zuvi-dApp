df7e8d0ebb0306e5d8d9953984acb375ef1b0bee
silentoaq <silentoaq@gmail.com>
2025-07-19 19:29:14 +0800

    chore : 清空 web

--- src/web/twattest-sdk/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { TwattestSDK } from './web.js';
-export { TwattestVerifier } from './node.js';
-export type { UserPermissions, TwfidoAttestation, TwlandAttestation, AttestationStatus } from './web.js';
-export type { TwattestSDKConfig, DataRequestConfig, DataRequestSession } from './web.js';
-export type { TwattestVerifierConfig, VerificationResult, ExtractedData, DataSchema } from './node.js';
-//# sourceMappingURL=index.d.ts.map
